62527f03e2326a81dfcf5855efc2d262a36f504d
Tanish Garge <=>
2024-08-25 20:13:24 +0530

    fix: typo

--- src/pages/courses/CreateCourse.js
+++ src/pages/courses/CreateCourse.js
@@ -62,7 +62,7 @@ export default function CreateCourse() {
                         marginTop: 8,
                         display: "flex",
                         flexDirection: "column",
-                        alignItems: "center",val
+                        alignItems: "center",
                     }}
                 >
                     <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
